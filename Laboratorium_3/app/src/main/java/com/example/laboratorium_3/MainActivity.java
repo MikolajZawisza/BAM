03a373a2dcc57207b135b074c4be5ca9d51b8c74
Mikołaj <mikolaj.zawisza.96@gmail.com>
2022-03-24 18:35:12 +0100

    Get Request compleated

--- Laboratorium_3/app/src/main/java/com/example/laboratorium_3/MainActivity.java
+++ Laboratorium_3/app/src/main/java/com/example/laboratorium_3/MainActivity.java
@@ -7,61 +7,68 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import android.util.Log;
+import android.widget.Button;
+
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Button get_request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void getRequest (View view) {
-        Thread t = new Thread() {
+        get_request = (Button) findViewById(R.id.get_request);
+        get_request.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                super.run();
+            public void onClick(View view) {
+                Thread t = new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
 
-                // TODO Exceptions handling
+                        // TODO Exceptions handling
 
-                try {
-                    // From https://www.baeldung.com/java-http-request
-                    // 1. Creating a request
-                    URL url = new URL("https://jsonplaceholder.typicode.com/posts");
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    con.setRequestMethod("GET");
+                        try {
+                            // From https://www.baeldung.com/java-http-request
+                            // 1. Creating a request
+                            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+                            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                            con.setRequestMethod("GET");
 
-                    // 5. Setting request headers
-                    con.setRequestProperty("Content-Type", "application/json");
+                            // 5. Setting request headers
+                            con.setRequestProperty("Content-Type", "application/json");
 
-                    // 6. Configuring timeouts
-                    con.setConnectTimeout(5000);
-                    con.setReadTimeout(5000);
+                            // 6. Configuring timeouts
+                            con.setConnectTimeout(5000);
+                            con.setReadTimeout(5000);
 
-                    // 8. Reading the response
-                    int status = con.getResponseCode();
+                            // 8. Reading the response
+                            int status = con.getResponseCode();
 
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(con.getInputStream()));
-                    String inputLine;
-                    StringBuffer content = new StringBuffer();
-                    while ((inputLine = in.readLine()) != null) {
-                        content.append(inputLine);
+                            BufferedReader in = new BufferedReader(
+                                    new InputStreamReader(con.getInputStream()));
+                            String inputLine;
+                            StringBuffer content = new StringBuffer();
+                            while ((inputLine = in.readLine()) != null) {
+                                content.append(inputLine);
+                            }
+                            in.close();
+
+                            con.disconnect();
+
+                            Log.d("Zwrócone dane:", content.toString());
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
-                    in.close();
-
-                    con.disconnect();
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
+                };
+                t.start();
             }
-        };
-        t.start();
+        });
     }
-
-
-
-
 }
